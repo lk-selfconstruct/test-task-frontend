ceb780ab0e06d84e464aab60611347503bce650a
Maxim Novikov <maxim.novikov@onlinesup.com>
2025-06-09 19:13:13 +0300

    add loader and loader wrapper

---
+++ src/shared/ui/Loader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="fixed inset-0 z-50 flex items-center justify-center bg-black/60">
+    <span
+      class="inline-block w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"
+    ></span>
+  </div>
+</template>
---
+++ src/shared/ui/LoaderWrapper.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Loader v-if="isLoading" />
+    <slot v-else />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Loader from './Loader.vue'
+
+const { isLoading } = defineProps<{
+  isLoading: boolean
+}>()
+</script>
