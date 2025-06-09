534a0e9e65cb61ae52adcd4d460580c751df0ba5
Maxim Novikov <maxim.novikov@onlinesup.com>
2025-06-09 18:32:35 +0300

    add @tanstack/vue-query, add getStore request

--- src/modules/Main/pages/MainPage.vue
+++ src/modules/Main/pages/MainPage.vue
@@ -2,6 +2,14 @@
   <div>MainPage</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query'
+import { getStore } from '@/modules/Main/api/store'
+
+const { data, isLoading } = useQuery({
+  queryKey: ['store'],
+  queryFn: getStore,
+})
+</script>
 
 <style scoped></style>
